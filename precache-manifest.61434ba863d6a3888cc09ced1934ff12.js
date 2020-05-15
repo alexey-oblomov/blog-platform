c13443ce786c100a8fb934e3d26b2e2111262722
Alexey Oblomov <alexey.oblomov@mail.ru>
2020-05-15 14:21:42 +0300

    Updates

---
+++ precache-manifest.61434ba863d6a3888cc09ced1934ff12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332dddd1a9393dff6e89a6119323c5b6",
+    "url": "/blog-platform/index.html"
+  },
+  {
+    "revision": "811878506464d054724b",
+    "url": "/blog-platform/static/css/main.3944b34a.chunk.css"
+  },
+  {
+    "revision": "712c1fb53d6edf78f0a7",
+    "url": "/blog-platform/static/js/2.fa9cbd50.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/blog-platform/static/js/2.fa9cbd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811878506464d054724b",
+    "url": "/blog-platform/static/js/main.b549d384.chunk.js"
+  },
+  {
+    "revision": "75649d5d952ef8339f07",
+    "url": "/blog-platform/static/js/runtime-main.b928ca90.js"
+  }
+]);
