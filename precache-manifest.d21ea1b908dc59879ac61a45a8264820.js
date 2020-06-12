20785e88544459b008e8d57a05d2f690b6cd9850
Alexey Oblomov <alexey.oblomov@mail.ru>
2020-06-13 00:57:55 +0300

    Updates

--- precache-manifest.d21ea1b908dc59879ac61a45a8264820.js
+++ precache-manifest.d21ea1b908dc59879ac61a45a8264820.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4157fb79468211b57b2799d6ed84925a",
+    "revision": "3e831933d715a5ea565d35cb7a1a7b4a",
     "url": "/blog-platform/index.html"
   },
   {
-    "revision": "bdb35c4b190b528b1773",
+    "revision": "b169b1548fe1671995b1",
     "url": "/blog-platform/static/css/main.64553ad7.chunk.css"
   },
   {
-    "revision": "ad59693afcf0a70fd756",
-    "url": "/blog-platform/static/js/2.3d0fded9.chunk.js"
+    "revision": "4a277f327134ac0b4a80",
+    "url": "/blog-platform/static/js/2.ce1e0642.chunk.js"
   },
   {
     "revision": "6b8f5144dc7033ecceae1876faf39fca",
-    "url": "/blog-platform/static/js/2.3d0fded9.chunk.js.LICENSE.txt"
+    "url": "/blog-platform/static/js/2.ce1e0642.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bdb35c4b190b528b1773",
-    "url": "/blog-platform/static/js/main.4fa0a6cd.chunk.js"
+    "revision": "b169b1548fe1671995b1",
+    "url": "/blog-platform/static/js/main.dc84defa.chunk.js"
   },
   {
     "revision": "75649d5d952ef8339f07",
